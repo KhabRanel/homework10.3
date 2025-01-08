1c568b0ff8dcae11e693edf2160712eb820727cf
KhabRanel <habibullin.ran@gmail.com>
2025-01-08 15:00:28 +0300

    Add files via upload

---
+++ homework10.3.py
@@ -0,0 +1,40 @@
+import threading
+import random
+import time
+
+
+class Bank:
+    def __init__(self, balance=0):
+        self.balance = balance
+        self.lock = threading.Lock()
+
+    def deposit(self):
+        for i in range(100):
+            dep = random.randint(50, 500)
+            self.balance += dep
+            print(f"Пополнение: {dep}. Баланс: {self.balance}")
+            if self.balance >= 500 and self.lock.locked():
+                self.lock.release()
+            time.sleep(0.001)
+
+    def take(self):
+        for i in range(100):
+            tk = random.randint(50, 500)
+            print(f"Запрос на {tk}")
+            if tk <= self.balance:
+                self.balance -= tk
+                print(f"Снятие: {tk}. Баланс: {self.balance}")
+            else:
+                print("Запрос отклонён, недостаточно средств")
+                self.lock.acquire()
+            time.sleep(0.001)
+
+
+bk = Bank()
+th1 = threading.Thread(target=Bank.deposit, args=(bk,))
+th2 = threading.Thread(target=Bank.take, args=(bk,))
+th1.start()
+th2.start()
+th1.join()
+th2.join()
+print(f"Итоговый баланс: {bk.balance}")
